b339bfb6030c2b4c5035fcfe7c73050fd1b15b3d
zahngsan <zhang@itcast.cn>
2020-08-08 12:20:13 +0800

    HelloPrint version 3

--- GitTest/src/bigdata/itcast/cn/HelloPriint.java
+++ GitTest/src/bigdata/itcast/cn/HelloPriint.java
@@ -4,5 +4,6 @@ public class HelloPriint {
     public static void main(String[] args) {
         System.out.println("this is version 1, I love China");
         System.out.println("this is version 2, I love itcast");
+        System.out.println("this is version 3, I love heima");
     }
 }
